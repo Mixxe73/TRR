6a7ef5eb74fad9bc4fc0361fcbb1e4df3a102387
Mixxe73 <51820460+Mixxe73@users.noreply.github.com>
2020-10-26 10:32:14 +0400

    Update MultipicationAmmo
    Добавил возможность выдавать рейты x4 по рангам

--- TRRMultiplicationAmmo.cs
+++ TRRMultiplicationAmmo.cs
@@ -7,16 +7,30 @@ using Oxide.Core.Libraries;
 
 namespace Oxide.Plugins
 {
-    [Info("TRRMultiplicationAmmo", "Mixxe73", "1.0.0")]
+    [Info("TRRMultiplicationAmmo", "Mixxe73", "1.2.0")]
     class TRRMultiplicationAmmo : RustLegacyPlugin
     {
 		private void OnItemCraft(CraftingInventory inventory, BlueprintDataBlock blueprint, int amount, ulong startTime)
         {
+
+            int[] Improved_ranks = new int[] { 4, 3 }; //Ранги с рейтами X4
             var netUser = inventory.GetComponent<Character>().netUser;
+            RustExtended.UserData userData = Users.GetBySteamID(netUser.userID);
+
             if (blueprint.resultItem.name == "556 Ammo" || blueprint.resultItem.name == "9mm Ammo" || blueprint.resultItem.name == "Shotgun Shells")
             {
+                if (Array.IndexOf(Improved_ranks, userData.Rank) == 1)
+                {
+                int NewCount = amount + amount + amount;
+                inventory.AddItemAmount(DatablockDictionary.GetByName(blueprint.resultItem.name), NewCount);
+                return;
+                }
+                else
+                {
                 int NewCount = amount + amount;
                 inventory.AddItemAmount(DatablockDictionary.GetByName(blueprint.resultItem.name), NewCount);
+                return;
+                }
             }
             else
             {
