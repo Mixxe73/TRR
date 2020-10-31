89a375aec4a5c64cbbba93d448873b80a4bc448e
Mixxe73 <51820460+Mixxe73@users.noreply.github.com>
2020-10-31 18:24:59 +0400

    1.2.5
    Переименовал плагин из TRRMultiplicationAmmo в TRRExpFeatures
    
    Добавил возможность добычи из статического дерева

--- TRRExpFeatures.cs
+++ TRRExpFeatures.cs
@@ -4,22 +4,23 @@ using Oxide.Core;
 using System.Collections.Generic;
 using RustExtended;
 using Oxide.Core.Libraries;
-
+//плагин позволяет крафтить по много патронов, добывать дерево по другому кол-ву
 namespace Oxide.Plugins
 {
-    [Info("TRRMultiplicationAmmo", "Mixxe73", "1.2.0")]
-    class TRRMultiplicationAmmo : RustLegacyPlugin
+    [Info("TRR Experemental Features", "Mixxe73", "1.2.5")]
+    class TRRExpFeatures : RustLegacyPlugin
     {
-		private void OnItemCraft(CraftingInventory inventory, BlueprintDataBlock blueprint, int amount, ulong startTime)
-        {
-
-            int[] Improved_ranks = new int[] { 4, 5 }; //Ранги с рейтами X4
+        int[] Improved_ranks = new int[] { 4, 5 }; //Ранги с рейтами X4
+            int STWood = 12; //Сколько выдавать дерева при добыче статического дерева
 
             /*
             Текущие ранги: 
             Ранг 5 - Боров (Самый сок)
             Ранг 4 - Мастер
             */
+
+		private void OnItemCraft(CraftingInventory inventory, BlueprintDataBlock blueprint, int amount, ulong startTime)
+        {
             var netUser = inventory.GetComponent<Character>().netUser;
             RustExtended.UserData userData = Users.GetBySteamID(netUser.userID);
 
@@ -43,5 +44,17 @@ namespace Oxide.Plugins
                 return;
             }           
         } 
+
+        int OnGather(Inventory inventory, ResourceTarget resourceTarget, object obj, int num)
+        {
+            if(obj == null)
+            {
+                var netUser = inventory.GetComponent<Character>().netUser;
+                rust.InventoryNotice(netUser, STWood + " x Wood");
+                inventory.AddItemAmount(DatablockDictionary.GetByName("Wood"), STWood);
+            }
+            return num;
+        }
+
 	}
 }
