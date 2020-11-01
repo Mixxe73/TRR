047befd8643e7cf453a7c733c955a55d7a379bba
Mixxe73 <51820460+Mixxe73@users.noreply.github.com>
2020-11-01 18:40:17 +0300

    RPC_SETONL
    RPC_SETONL

---
+++ TRRSetOnline.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+using RustExtended;
+
+namespace Oxide.Plugins
+{
+    [Info("SetOnline", "Romanchik34", "1.0.0")]
+    class TRRSetOnline : RustLegacyPlugin
+    {
+        void Loaded()
+        {
+            timer.Repeat(10f, 0, () =>
+            {
+                foreach (PlayerClient player in PlayerClient.All)
+                {
+                    player.networkView.RPC("SetOnline", player.netPlayer, "TRRMod.ShowOnline.SetOnline", PlayerClient.All.Count, server.maxplayers);
+                }
+            });
+        }
+    }
+}
